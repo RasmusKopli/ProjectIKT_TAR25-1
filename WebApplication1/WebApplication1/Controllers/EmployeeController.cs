19969f45974e72ef6626e8da0b244d3421f72141
RasmusKopli <kopli.rasmus@gmail.com>
2026-06-06 10:25:30 +0300

    EmployeeDetailsViewModel, Details view, index code fixes

--- WebApplication1/WebApplication1/Controllers/EmployeeController.cs
+++ WebApplication1/WebApplication1/Controllers/EmployeeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 using Projekt.Data;
+using Projekt.ViewModels.EmployeeVM;
 
 namespace Projekt.Controllers
 {
@@ -16,6 +18,26 @@ namespace Projekt.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.Employees.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var employee = await _context.Employees.FirstOrDefaultAsync(m =>  m.Id == id);
+
+            if (employee == null) return NotFound();
+
+            var viewModel = new EmployeeDetailsViewModel
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                Position = employee.Position,
+                Reputation = employee.Reputation,
+                Wage = employee.Wage
+            };
+
+            return View(viewModel);
         }
     }
 }
