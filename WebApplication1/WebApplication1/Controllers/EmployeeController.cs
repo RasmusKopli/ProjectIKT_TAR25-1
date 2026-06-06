283ee14360cfc0e284709c60d22a2cbb467e43b9
RasmusKopli <kopli.rasmus@gmail.com>
2026-06-06 09:22:24 +0300

    Employee view and page work now!! :)

---
+++ WebApplication1/WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Projekt.Data;
+
+namespace Projekt.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly WorkplaceContext _context;
+
+        public EmployeeController(WorkplaceContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Employees.ToListAsync());
+        }
+    }
+}
