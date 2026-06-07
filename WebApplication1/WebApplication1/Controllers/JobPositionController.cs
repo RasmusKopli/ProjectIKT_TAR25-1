359ae58402a02459efceac0528ded03ebbc01100
RasmusKopli <kopli.rasmus@gmail.com>
2026-06-07 18:21:52 +0300

    Made apply button, instead of edit, could probably return later if i still have time

--- WebApplication1/WebApplication1/Controllers/JobPositionController.cs
+++ WebApplication1/WebApplication1/Controllers/JobPositionController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Projekt.Data;
+using Projekt.Models;
+using Projekt.ViewModels.JobPositionVM;
 
 namespace Projekt.Controllers
 {
@@ -16,6 +18,62 @@ namespace Projekt.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.JobPositions.ToListAsync());
+        }
+
+
+        public async Task<IActionResult> Apply(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var position = await _context.JobPositions.FindAsync(id);
+
+            if (position == null) return NotFound();
+
+            if (!position.IsAvailable)
+                return RedirectToAction(nameof(Index));
+
+            var viewModel = new JobPositionApplyViewModel
+            {
+                JobPositionId = position.Id,
+                JobName = position.JobName
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Apply (JobPositionApplyViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var position = await _context.JobPositions.FindAsync(viewModel.JobPositionId);
+
+                if (position == null) return NotFound();
+
+                if (!position.IsAvailable)
+                {
+                    ModelState.AddModelError("", "This position is no longer available.");
+                    return View(viewModel);
+                }
+
+                var employee = new Employee
+                {
+                    Name = viewModel.Name,
+                    Reputation = viewModel.Reputation,
+                    Wage = viewModel.Wage,
+                    JobPositionId = viewModel.JobPositionId,
+                    Position = viewModel.JobName
+                };
+
+                    position.IsAvailable = false;
+
+                _context.Employees.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(viewModel);
         }
     }
 }
---
+++ WebApplication1/WebApplication1/ViewModels/JobPositionVM/JobPositionApplyViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace Projekt.ViewModels.JobPositionVM
+{
+    public class JobPositionApplyViewModel
+    {
+        public int JobPositionId { get; set; }
+
+        [ValidateNever]
+        public string JobName { get; set; }
+
+        public string Name { get; set; }
+        public string Reputation { get; set; }
+        public int Wage { get; set; }
+    }
+}
